63b008b487aed8c7a960b44a1734b7479225f8de
IBARAM1917 <abiraminagaraj22@gmail.com>
2024-07-05 13:42:08 +0530

    completed

--- Routers/checkoutRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {createOrder} from '../Controllers/checkoutController.js'
-
-
-const router =express.Router();
-
-router.post('/payment',createOrder);
-
-module.exports={router};
